45ce0f2e72a837849d5efc187de7b6571d42d52c
Zachary <zachary.15pierce@gmail.com>
2019-09-06 16:53:22 -0600

    Centered Quote and Sign in buttons and added css elements

--- client/src/components/mainPage.js
+++ client/src/components/mainPage.js
@@ -1,36 +1,114 @@
 import React from 'react';
 import  { css } from '@emotion/core';
 
+import SignUp from './signUp';
 
 class MainPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      signIn: false,
+      signUp: false,
+    }
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
 
+  onClickHandler(e) {
+    if(e.target.name === 'sign up') {
+      this.setState({ signUp: true })
+    }
+    if(e.target.name === 'sign in') {
+      console.log('THis is the best this ever')
     }
   }
   render() {
-    return (
-    <div>
-      <div css={quote}>
-        There's No Such Thing As A Bad Day.
-      </div> 
-      <div>
-        Just Days You Haven't Looked Hard Enough For The Good In It.
-      </div>
-      <button>
-        Sign In
-      </button>
-      <button>
-        Sign Up
-      </button>
-    </div>
-    )
+    const { signIn, signUp } = this.state
+
+    if(!signIn && !signUp){
+      return (
+        <div css={homePage}>
+
+          <div>
+
+            <div css={quote}>
+              "There's No Such Thing As A Bad Day.
+            </div> 
+
+            <div css={middleQuote}>
+              Just Days You Haven't
+            </div>
+
+            <div css={quote}>
+              Looked Hard Enough For The Good In It."
+            </div>
+
+            <div css={author}>
+              ~ Zachary Michael Pierce 
+            </div>
+
+            <div css={buttons}>
+              <button css={eachButton} onClick={this.onClickHandler} name='sign in'>
+                Sign In
+              </button>
+              <button css={eachButton} onClick={this.onClickHandler} name='sign up'>
+                Sign Up
+              </button>  
+            </div>
+
+          </div>
+
+        </div>
+        )
+    }
+    if(signUp) {
+      return (
+        <SignUp />
+      )
+    }
   }
 }
+
 // ========  CSS Emotions ========
 
 const quote = css`
-  color: red;
+  font-size: 55px;
+  color: grey;
+  margin-top: 10px;
+  text-align: left;
+`
+const middleQuote = css`
+  font-size: 55px;
+  color: grey;
+  margin-top: 10px;
+  text-align: center;
+`
+const author = css`
+  font-size: 32px;
+  text-align: right;
+  margin-top: 10px;
+`
+const homePage = css`
+  margin-top: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const eachButton = css`
+  background-color: blue;
+  border: none;
+  color: white;
+  padding: 20px 37px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 15px;
+  margin-left: 10px;
+  border-radius: 12px;
+`
+const buttons = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 export default MainPage
---
+++ client/src/components/signUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import css from '@emotion/core';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={}
+  }
+  render() {
+    return (
+      <div>
+        This is SIGN UP 
+      </div>
+    )
+  }
+}
+export default SignUp

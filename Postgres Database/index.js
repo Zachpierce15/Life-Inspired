432413ec69e8d1c04d09d6a80c784567da9a08b3
Zachary <zachary.15pierce@gmail.com>
2019-09-17 13:26:45 -0600

    Can now save users to the database

--- Postgres Database/index.js
+++ Postgres Database/index.js
@@ -12,26 +12,41 @@ pool.connect()
   .catch(err => console.log(err));
 
 // ====== POST Request ======= 
-  // pool.query(`INSERT INTO users (username, email) VALUES ('Haley Jenkins', 'Haley@gmail.com')`, (err, data) => {
-  //   if(err) {
-  //     console.log('This is ERROR', err);
-  //   } else {
-  //     console.log('POST Successful');
-  //   }
-  // });
+  const addUsers = (userData, cb) => {
+    console.log('This is USER DATA', userData)
+    const { displayName, username, email, password } = userData
+    console.log('This is accessing displayname', displayName)
+    pool.query(`INSERT INTO users (username, email, password, displayname) VALUES ('${username}', '${email}', '${password}', '${displayName}')`, (err, data) => {
+      if(err) {
+        console.log('This is an error', err)
+      } else {
+        cb(null, data)
+      }
+    })
+  }
 // ====== Get Request =======
-  // pool.query(`SELECT * FROM messages WHERE user_id=1`, (err, messages) => {
-  //   if(err) {
-  //     console.log('Error Retrieving Data', err);
-  //   } else {
-  //     console.log('Messages', messages);
-  //   }
-  // });
+  const getMessages = () => {
+    pool.query(`SELECT * FROM messages WHERE user_id=1`, (err, messages) => {
+      if(err) {
+        console.log('Error Retrieving Data', err);
+      } else {
+        console.log('Messages', messages);
+      }
+    });
+  }
 // ======== DELETE Request ========
-  pool.query(`DELETE FROM messages WHERE id=2`, (err,data) => {
-    if(err) {
-      console.log('ERROR DELETE ATTEMPT', err);
-    } else {
-      console.log('SUCCESSFUL DELETE', data);
-    }
-  });
+  const deleteMessages = () => {
+    pool.query(`DELETE FROM messages WHERE id=2`, (err,data) => {
+      if(err) {
+        console.log('ERROR DELETE ATTEMPT', err);
+      } else {
+        console.log('SUCCESSFUL DELETE', data);
+      }
+    });
+  }
+
+  module.exports = {
+    addUsers, 
+    getMessages,
+    deleteMessages
+  }
--- server/server.js
+++ server/server.js
@@ -4,15 +4,24 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const model = require('../Postgres Database/index.js')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../dist'));
 
+// ========= Get Routes =========
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// ======== Post Routes ========
 app.post('/newUser', (req, res) => {
-  console.log('this is data coming in', req.body)
-  res.send('This is getting here SERVER')
+  model.addUsers(req.body, (err, data) => {
+    if(err) {
+      res.status(400).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
